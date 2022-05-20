623ed01f8eb253b4890be5f35fc3c6cef5871eef
KANGDONGHOON0413 <kmerits@gmail.com>
2022-05-20 23:23:09 +0900

    초기 커밋.

---
+++ day06_01/Computer01.cs
@@ -0,0 +1,33 @@
+﻿#define ALPHA   //이 파일에서만 사용가능
+#undef DEBUG    //DEBUG는 사용하지 않겠다는 의미
+
+using System;
+using static day06_01.Computer02;
+
+//전처리기 연습
+namespace day06_01
+{
+    class Computer01
+    {
+        
+        static void Main(string[] args)
+        {
+            bool verbose = false;
+#if(ALPHA)
+            Console.WriteLine("test environment: verbose opthion is set");
+            verbose = true;
+#else
+            Console.WriteLine("production");
+#endif
+            if (verbose)
+            {
+                
+                Console.WriteLine("verbose mode");
+#if(!DEBUG)
+                var Server = Computer02.Server();
+                Console.WriteLine(Server);
+#endif
+            }
+        }
+    }
+}
---
+++ day06_01/Computer02.cs
@@ -0,0 +1,22 @@
+﻿#define BETA
+using System;
+
+namespace day06_01
+{
+    public class Computer02
+    {
+        public static string Server()
+        {
+            string server;
+#if(ALPHA)
+            server = "Alpha testing....";
+
+#elif (BETA)
+            server = "Beta testing....";
+#else
+            server = "Operating....";
+#endif
+            return server;
+        }
+    }
+}
